08239b09f0abb3a24ae5a1a1bc7ca857f2be0db2
Nilesh <nil.y.jadhav@gmail.com>
2023-02-04 11:16:37 +0800

    inital commit

---
+++ src/main/java/com/nus/iss/workshop11041/workshop11041/Workshop11041Application.java
@@ -0,0 +1,13 @@
+package com.nus.iss.workshop11041.workshop11041;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Workshop11041Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Workshop11041Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/nus/iss/workshop11041/workshop11041/Workshop11041ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nus.iss.workshop11041.workshop11041;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Workshop11041ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
